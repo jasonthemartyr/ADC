7bddbfca53fb51272d68ffa4ab94e9f375c411ed
Jason Marter <jason.marter@gmail.com>
2017-09-20 12:09:36 -0500

    Update vip_build.py

--- F5/vip_build.py
+++ F5/vip_build.py
@@ -10,7 +10,8 @@ virtual_address = ltm.virtual_address_s
 virtual_servers = ltm.virtuals
 get_nodes = ltm.nodes.get_collection()
 
-#unpack collection
+
+# unpack collection
 # for server in virtual_servers.get_collection():
 #     print(server.raw)
 
@@ -21,12 +22,19 @@ get_nodes = ltm.nodes.get_collection()
 # existing_nodes = mgmt.tm.ltm.nodes.get_collection()
 
 
-#build node
+# build node
 # test_jason_node = ltm.nodes.node.create(partition='CDE-DMZ',name='test_jason01', address='10.98.10.254', description='this is a test node created via the SDK')
 # print(test_jason_node.raw)
 
-def build_node(f5_partition,node_name,node_address,node_desc):
-
+def build_node(f5_partition, node_name, node_address):
+    """
+    builds an F5 node
+    :param f5_partition:
+    :param node_name:
+    :param node_address:
+    :param node_desc:
+    :return:
+    """
     try:
         existing_nodes = mgmt.tm.ltm.nodes.get_collection()
     except ValueError:
@@ -36,13 +44,21 @@ def build_node(f5_partition,node_name,node_address,node_desc):
         if node_name in node_list:
             print("{} exists".format(node_name))
         else:
-            node = mgmt.tm.ltm.nodes.node.create(partition=f5_partition, name=node_name, address=node_address,description=node_desc)
+            node_desc = '{}.werner.com'.format(node_name)
+            node = mgmt.tm.ltm.nodes.node.create(partition=f5_partition, name=node_name, address=node_address,
+                                                 description=node_desc)
             return node.raw
 
 
-def build_pool(f5_partition, pool_name, pool_desc):
-    # create pool
-
+def build_pool(f5_partition, pool_name, node_list, allow_nat, allow_snat, ramp_time, monitor):
+    """
+    builds F5 pool and adds nodes specified in list to pool
+    :param f5_partition:
+    :param pool_name:
+    :param node_list:
+    :return:
+    """
+    # create pool and add nodes
     try:
         existing_pools = mgmt.tm.ltm.pools.get_collection()
     except ValueError:
@@ -52,67 +68,135 @@ def build_pool(f5_partition, pool_name, pool_desc):
         if pool_name in pool_list:
             print("{} exists".format(pool_name))
         else:
+            pool_desc = '{}.werner.com'.format(pool_name)
+            monitor_path = '/Common/{}'.format(monitor)
+            pool = mgmt.tm.ltm.pools.pool.create(partition=f5_partition, name=pool_name, description=pool_desc,allowNat=allow_nat,allowSnat=allow_snat, slowRampTime=ramp_time, monitor=monitor_path)
+            my_pool = mgmt.tm.ltm.pools.pool.load(partition=f5_partition, name=pool_name)
+            for node in node_list:
+                add_nodes = my_pool.members_s.members.create(partition=f5_partition, name=node)
+                test = my_pool.members_s.get_collection()
+                for x in test:
+                    print(x.raw)
 
-            pool = mgmt.tm.ltm.pools.pool.create(partition=f5_partition, name=pool_name, description=pool_desc)
-            return pool.raw
+                print('{} was added to {}'.format(add_nodes.name, pool.name))
 
 
-#function needs testing
-def f5_post_builder(object_name, collection_name, post_cmd):
+# return pool.raw
+
+
+
+
+def build_vip(f5_partition, vip_name, vip_ip, vip_port, pool_name):
     try:
-        existing_collection = 'mgmt.tm.ltm.' + collection_name + '.get_collection()'
+        existing_vips = mgmt.tm.ltm.virtuals.get_collection()
     except ValueError:
         print("Could not connect to F5")
     else:
-        object_list = [object.name for object in existing_collection]
-        if object_name in object_list:
-            print("{} exists".format(object_name))
+        vip_list = [vip.name for vip in existing_vips]
+        if vip_name in vip_list:
+            print("{} exists".format(vip_name))
         else:
-            post = post_cmd
-            return post.raw
+            vip_desc = '{}.werner.com'.format(vip_name)
+            vip_pool = '/{}/{}'.format(f5_partition, pool_name)
+            vip_dest = '/{}/{}%10:{}'.format(f5_partition, vip_ip, vip_port)
+
+            # node = mgmt.tm.ltm.nodes.node.create(partition=f5_partition, name=node_name, address=node_address,description=vip_desc)
+            vip = ltm.virtuals.virtual.create(partition=f5_partition, name=vip_name, description=vip_desc,
+                                              destination=vip_dest, pool=vip_pool)
+            return vip.raw
 
 
+# test_jason_vip = ltm.virtuals.virtual.create(partition='CDE-DMZ', name='test_jason_http-80', description='this is a test VIP created via the SDK', destination='/CDE-DMZ/10.98.0.254%10:80', pool='/CDE-DMZ/test_jason_http_80')
 
-f5_partition = 'CDE-DMZ'
-node_name = 'test_jason01'
-node_address = '10.98.10.254'
-node_desc = 'this is a test node created via the SDK'
-pool_name = 'test_jason_http_80'
-pool_desc = 'this is a test pool created via the SDK'
-
-# ww = build_node(f5_partition,node_name,node_address,node_desc)
+# existing_nodes = mgmt.tm.ltm.virtuals.get_collection()
 #
-# print(ww)
-zz = build_pool(f5_partition, pool_name, pool_desc)
-print(zz)
-
-
-# x = mgmt.tm.ltm.pools.get_collection()
 #
-# for y in x:
-#     print(y.raw)
+# for node in existing_nodes:
+#     print(node.name)
 
 
 
 
-    # print(test_jason_pool.raw)
 
-#load an existing pool
+
+# load an existing pool
 # test_jason_pool = mgmt.tm.ltm.pools.pool.load(partition='CDE-DMZ', name='test_jason_http_80')
 # test_jason_pool_member = test_jason_pool.members_s.members
 
-#add node to pool
-# test_jason_pool_member_add = test_jason_pool_member.create(partition='CDE-DMZ', name='test_jason01:80')
+#
 
-#get pool memebers
+# add node to pool
+# test_jason_pool_member_add = test_jason_pool_member.create(partition='CDE-DMZ', name='test_jason01:80')
+# test_jason_pool.members_s.members.create(partition='CDE-DMZ', name='test_jason01:80')
+# get pool memebers
 # get_test_jason_pool_members = test_jason_pool.members_s.get_collection()
 # print(test_jason_pool_member.raw)
 
-#build VIP and add pool
+
+
+# test = mgmt.tm.ltm.pools.get_collection()
+#
+# for pool in test:
+#
+#     print(pool.name)
+#     for member in pool.members_s.get_collection():
+#
+#         print(member.name)
+
+
+f5_partition = 'CDE-DMZ'
+node_dict = {'test_jason01': '10.98.10.252', 'test_jason02': '10.98.10.253', 'test_jason03': '10.98.10.254'}
+pool_name = 'test_jason_http_80'
+node_list = ['test_jason01:80', 'test_jason02:443']
+vip_name = 'test_jason_http-80'
+vip_ip = '10.98.0.254'
+vip_port = '80'
+
+allow_nat = 'no'
+allow_snat = 'no'
+ramp_time = '5'
+monitor = 'tcp'
+
 # test_jason_vip = ltm.virtuals.virtual.create(partition='CDE-DMZ', name='test_jason_http-80', description='this is a test VIP created via the SDK', destination='/CDE-DMZ/10.98.0.254%10:80', pool='/CDE-DMZ/test_jason_http_80')
-#print(test_jason_vip.raw)
+
+# loop to build nodes
+for node_name, node_address in node_dict.items():
+    building_nodes = build_node(f5_partition, node_name, node_address)
+    #print(building_nodes)
+
+#zz = build_pool(f5_partition, pool_name, node_list)
+zz = build_pool(f5_partition, pool_name, node_list, allow_nat, allow_snat, ramp_time, monitor)
+
+print(zz)
+
+# vip = build_vip(f5_partition, vip_name, vip_ip, vip_port, pool_name)
+
+vip = build_vip(f5_partition, vip_name, vip_ip, vip_port, pool_name)
+print(vip)
+
+
+# existing_pools = mgmt.tm.ltm.pools.get_collection()
+#
+#
+# for pool in existing_pools:
+#     print(pool.raw)
 
 
 
 
 
+
+# load an existing pool
+# test_jason_pool = mgmt.tm.ltm.pools.pool.load(partition='CDE-DMZ', name='test_jason_http_80')
+# test_jason_pool_member = test_jason_pool.members_s.members
+
+# add node to pool
+# test_jason_pool_member_add = test_jason_pool_member.create(partition='CDE-DMZ', name='test_jason01:80')
+
+# get pool memebers
+# get_test_jason_pool_members = test_jason_pool.members_s.get_collection()
+# print(test_jason_pool_member.raw)
+
+# build VIP and add pool
+# test_jason_vip = ltm.virtuals.virtual.create(partition='CDE-DMZ', name='test_jason_http-80', description='this is a test VIP created via the SDK', destination='/CDE-DMZ/10.98.0.254%10:80', pool='/CDE-DMZ/test_jason_http_80')
+# print(test_jason_vip.raw)
